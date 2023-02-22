8fabaed48331b415c93ee60cecb7157d15f1a20b
unknown <sujeetgroups@gmail.com>
2023-02-22 16:02:09 +0530

    add a summary note

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/layoyt/Header';
+import Meals from './components/meals/Meals';
 
 function App() {
   return (
    <>
         <Header />
+        <Meals />
    </>
   );
 }
---
+++ src/components/meals/Meals.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MealsSummary from './MealsSummary'
+const Meals = () => {
+  return (
+    <>
+    <MealsSummary/ >
+    </>
+  )
+}
+
+export default Meals
